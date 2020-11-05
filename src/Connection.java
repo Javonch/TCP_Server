9b1cda41436e4d5c6a2b678fea0af9f3e10269c6
Javon Colvin-Hatchett <43857131+Javonch@users.noreply.github.com>
2020-11-05 15:15:15 -0500

    Bug Fixes
    Issue fixed where web_files didn't have a "/"
    Issue fixed where there would be infinite loop if more than 1 cookie

--- src/Connection.java
+++ src/Connection.java
@@ -4,7 +4,6 @@ import java.net.*;
 public class Connection extends Thread{
 	private InputStream in;
 	private OutputStream out;
-	
 	public Connection(Socket con) throws IOException {
 		this.in = con.getInputStream();
 		this.out = con.getOutputStream();
@@ -13,12 +12,13 @@ public class Connection extends Thread{
 	 * This method writes the correct header and hands off the work onto 
 	 * other abstracted methods
 	 */
+	
 	public void run() {
 		try {
 			PrintWriter printOut = new PrintWriter(out);
-			// Can't use Scanner because Java 7...
 			BufferedReader input = new BufferedReader(new InputStreamReader(in));
-			String reqType = input.readLine();
+			String reqType = input.readLine().trim();
+			System.out.println("request = " + reqType);
 			// Base case: If nothing in response. Do nothing.
 			if(reqType == null) {
 				printOut.close();
@@ -35,40 +35,47 @@ public class Connection extends Thread{
 			}
 			
 			else {
-				int spacePlace = reqType.indexOf(" ");
-				String website = reqType.substring(spacePlace, reqType.indexOf(" ", spacePlace + 1)).trim();
-				String fileName = website.substring(website.indexOf('/', 1));
+				String[] requests = reqType.split(" ");
+				String[] files = requests[1].split("/");
+				String fileName = "";
+				
+				for(String x: files) {
+					if(x.contains(".html"))
+						fileName = x;
+				}
+				
+				System.out.println(fileName);
 	
 				/* If we have a file request we actually use, then start writing that
 				 * file to them
 				 */
 				
-		if(fileName.equals("/test1.html") || fileName.equals("/test2.html")|| fileName.equals("/visits.html")) {
-			printOut.write("HTTP/ 1.1 200 OK \r\n");
-			printOut.write("Content type: text/html \r\n");
-			printOut.write("Keep-Alive: timeout=100 \r\n");
-			printOut.write("Connection: Keep-Alive, keep-alive \r\n");
-			int visits = TCP_Server.checkVisits(input, printOut, fileName);
+				if(fileName.equals("test1.html") || fileName.equals("test2.html")|| fileName.equals("visits.html")) {
+					System.out.println("You have a correct url");
+					printOut.write("HTTP/ 1.1 200 OK \r\n");
+					printOut.write("Content type: text/html \r\n");
+					printOut.write("Keep-Alive: timeout=10 \r\n");
+					printOut.write("Connection: Keep-Alive, keep-alive \r\n");
+					int visits = TCP_Server.checkVisits(input, printOut, fileName);
 			
-			if(fileName.equals("/visits.html")){
-				String sentence = "Your browser visited various URLs on this site " + visits + " times";
-				printOut.write("content-length: " + sentence.length());
-				printOut.write("\r\n\r\n");
-				printOut.write(sentence);
-			}
-			else
-				write(printOut,fileName);
-		}
-		
-		//Else just send the 404 Error and close up.
-		else {
-			writeError(printOut);
-		} 
-			printOut.flush();
-			printOut.close();
-			in.close();
-			out.close();
-			}
+					if(fileName.equals("visits.html")){
+						System.out.println("You are in the visit case");
+						String sentence = "Your browser visited various URLs on this site " + visits + " times";
+						printOut.write("content-length: " + sentence.length());
+						printOut.write("\r\n\r\n");
+						printOut.write(sentence);
+						}
+					else
+						write(printOut,fileName);
+					}
+				//Else just send the 404 Error and close up.
+				else 
+					writeError(printOut);
+				printOut.flush();
+				printOut.close();
+				in.close();
+				out.close();
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 				System.out.println("Connection Failed to Terminate");
@@ -81,7 +88,7 @@ public class Connection extends Thread{
 	 */
 	private static void write(PrintWriter pw, String _file) {
 		try {
-			String file = "web_files" + _file;
+			String file = "web_files/" + _file;
 			File f = new File(file);
 			BufferedReader br = new BufferedReader(new FileReader(f));
 			String line;
@@ -101,5 +108,6 @@ public class Connection extends Thread{
 	private static void writeError(PrintWriter pw) {
 		pw.write("HTTP/1.1 404 Not Found \r\n\r\n");
 		pw.write("404 Error: Page Not Found");
+		pw.close();
 	}
 }
--- src/TCP_Server.java
+++ src/TCP_Server.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 import java.util.Scanner;
 import java.io.*;
 public class TCP_Server {
+	
 	/*
 	 * Method that takes the requests from the client and puts them into a 
 	 * LinkedList in order to parse them easily
@@ -23,39 +24,50 @@ public class TCP_Server {
 	return requestList;
 	}
 	
-	public static void setCookie(String cookieName, String cookieVal, PrintWriter pw) {
+	public static void setCookie(String cookieName, int cookieVal, PrintWriter pw) {
 		pw.write("Set-Cookie: " + cookieName +"=" + cookieVal + " \r\n");
 	}
-	// Method to see if there is a visit to a certain cookie
-	//Created so i didn't have to hard code cookie names
-	public static String cookieCheck(BufferedReader requests, String cookieType) throws IOException {
+	
+	/*
+	 *  Method to see if there is a visit to a certain cookie
+	 *  Created so i didn't have to hard code cookie names
+	 */
+	
+	public static int cookieCheck(BufferedReader requests, String cookieType) throws IOException {
 		String looking = requests.readLine();
-		while(!looking.contains("Cookie") && !looking.equals("")) {
+		while(!looking.contains("Cookie") && !looking.isBlank()) {
 			looking = requests.readLine();
 		}
-		
-		while(looking.contains("Cookie")) {
-			if (looking.contains(cookieType)) {
-				return looking.substring(looking.indexOf("=") + 1);
+		if(looking.contains("Cookie")) {
+			String[] cookies = looking.split(";");
+			
+			for(String cookie: cookies) {
+				if(cookie.contains(cookieType)) {
+					return Integer.valueOf(cookie.substring(cookie.indexOf('=') + 1));
+				}
 			}
 		}
 		
-		return "-1";
+		
+		return -1;
 	}
+	
 	// method to check and increment visits to the sites
 	public static int checkVisits(BufferedReader br, PrintWriter pw, String fileName) throws NumberFormatException, IOException {
-		if (fileName.equals("/visits.html") || fileName.equals("/test1.html") || fileName.equals("/jtc131/test2.html")){
-			int visitCookie = Integer.parseInt(cookieCheck(br, "jtc131visits"));
+		if (fileName.equals("visits.html") || fileName.equals("test1.html") || fileName.equals("test2.html")){
+			int visitCookie = cookieCheck(br, "jtc131visits");
 			
 			if(visitCookie == -1) {
-				setCookie("jtc131visits", "1", pw);
+				setCookie("jtc131visits", 1, pw);
 				return 1;
 			}
-			setCookie("jtc131visits",String.valueOf(visitCookie + 1), pw);
+			setCookie("jtc131visits",visitCookie + 1, pw);
 			return visitCookie;
 					}
 		return -1;
 		}
+	
+	// Main method for server to initialize
 	public static void connect() throws IOException, InterruptedException {
 		Properties prop = new Properties();
 		FileInputStream ip = new FileInputStream("config.properties");
